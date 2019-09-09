90a02d18f0c914c953a93bc1219b2892f14d7488
20397 <20397@MININT-RTINT45.columbushs.com>
2019-09-09 09:27:06 -0400

    Done!

---
+++ src/pkgclass/schedule1/ClassSchedule1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule1;
+
+/**
+ *
+ * @author 20397
+ */
+public class ClassSchedule1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Grant Archer";
+        int year = 2020;
+        System.out.println("My name is " + name + " and i'll graduate in " + year);
+        String course1 = "English ";
+        String course2 = "Calculus";
+        String course3 = "AP CSA";
+        String course4 = "AP Physics";
+        String course5 = "AP Gov";
+        String course6 = "AP Micro";
+        String course7 = "Religion";
+        String teacher1 = "Mr. Lifors";
+        String teacher2 = "Mrs. Foyo";
+        String teacher3 = "Mr. Larrubia";
+        String teacher4 = "Mr. Sanchez";
+        String teacher5 = "Mrs. McCullagh";
+        String teacher6 = "Mr. Ciocca";
+        String teacher7 = "Mr. MARTIIIIIIII";
+        System.out.println("+--------------------------------------+");
+        System.out.println("| 1 |" + course1 + "   |  " + teacher1 + "       |");
+        System.out.println("| 2 |" + course2 + "   |  " + teacher2 + "        |");
+        System.out.println("| 3 |" + course3 + "     |  " + teacher3 + "     |");
+        System.out.println("| 4 |" + course4 + " |  " + teacher4 + "      |");
+        System.out.println("| 5 |" + course5 + "     |  " + teacher5 + "   |");
+        System.out.println("| 6 |" + course6 + "   |  " + teacher6 + "       |");
+        System.out.println("| 7 |" + course7 + "   |  " + teacher7 + " |");
+        System.out.println("+--------------------------------------+");
+    }
+}
